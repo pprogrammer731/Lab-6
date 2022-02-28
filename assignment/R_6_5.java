a9c78fb5772138993a46833f48e6cbe2fdb2da87
pprogrammer731 <94768931+pprogrammer731@users.noreply.github.com>
2022-03-01 00:40:24 +0300

    Add files via upload

---
+++ assignment/R_6_5.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+public class R_6_5 {
+    public static Stack <Integer>popAllRecursive(Stack stack) {
+
+        if(stack.isEmpty()) {
+            //nothing to remove, return
+            return null;
+        }
+        stack.pop();  // remove one stack element
+
+        popAllRecursive(stack); // recursive invocation of your method
+
+        return stack;
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack =new Stack<Integer>();
+        for (int i = 0; i <5 ; i++) {
+            stack.push(i);
+        }
+        System.out.println("Stack before popped:"+stack);
+        System.out.println("Stack elements are popped:"+popAllRecursive(stack));
+
+    }
+}
